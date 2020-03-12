f8f257674d385d7abe775d81b64bea057d261c7d
ninhxtina <christinaninh07@yahoo.com>
2020-03-11 21:47:28 -0700

    added tests

--- src/AutocompleteTests.java
+++ src/AutocompleteTests.java
@@ -7,7 +7,10 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class AutocompleteTests {
-    public static void main(String[] args) {
+
+
+    @Test
+    public void testOne() {
         Autocomplete.AutocompleteTrie search = new Autocomplete.AutocompleteTrie();
         search.insert("hello");
         search.insert("high");
@@ -16,16 +19,81 @@ public class AutocompleteTests {
         search.insert("see");
         search.insert("hollow");
         search.insert("how");
-        List<String> a = search.complete("h");
-        for(int i = 0; i < a.size(); i++) {
-            System.out.println(a.get(i));
-        }
+        List<String> e = search.complete("h");
 
+        for(int i = 0; i < e.size(); i++) {
+            assertEquals(e,e); //placeholder
+        }
     }
 
     @Test
-    public void test() {
+    public void testTwo() {
+        Autocomplete.AutocompleteTrie search = new Autocomplete.AutocompleteTrie();
+        search.insert("hello");
+        search.insert("high");
+        search.insert("seattle");
+        search.insert("seatac");
+        search.insert("see");
+        search.insert("hollow");
+        search.insert("how");
+        List<String> e = search.complete("se");
 
+        for(int i = 0; i < e.size(); i++) {
+            assertEquals(e,e); //placeholder
+        }
+    }
+
+    @Test
+    public void testThree() {
+        Autocomplete.AutocompleteTrie search = new Autocomplete.AutocompleteTrie();
+        search.insert("hello");
+        search.insert("high");
+        search.insert("seattle");
+        search.insert("seatac");
+        search.insert("see");
+        search.insert("hollow");
+        search.insert("how");
+        List<String> e = search.complete("sea");
+
+        for(int i = 0; i < e.size(); i++) {
+            assertEquals(e,e); //placeholder
+        }
+    }
+
+    @Test
+    public void testFour() {
+        Autocomplete.AutocompleteTrie search = new Autocomplete.AutocompleteTrie();
+        search.insert("hello");
+        search.insert("high");
+        search.insert("seattle");
+        search.insert("seatac");
+        search.insert("see");
+        search.insert("hollow");
+        search.insert("how");
+        List<String> e = search.complete("ho");
+
+        for(int i = 0; i < e.size(); i++) {
+            assertEquals(e,e); //placeholder
+        }
+    }
+
+    @Test
+    public void testNone() {
+        Autocomplete.AutocompleteTrie search = new Autocomplete.AutocompleteTrie();
+        search.insert("hello");
+        search.insert("high");
+        search.insert("seattle");
+        search.insert("seatac");
+        search.insert("see");
+        search.insert("hollow");
+        search.insert("how");
+        List<String> e = search.complete("xyz");
+
+        List<String> a = new ArrayList<>();
+
+        for(int i = 0; i < e.size(); i++) {
+            assertEquals(a,e); //placeholder
+        }
     }
 
 
