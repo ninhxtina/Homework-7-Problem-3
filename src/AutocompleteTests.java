bc98912c93768c3437f91d96c673da6eee896b0e
ninhxtina <christinaninh07@yahoo.com>
2020-03-11 20:26:07 -0700

    added to tests

--- src/AutocompleteTests.java
+++ src/AutocompleteTests.java
@@ -1,2 +1,29 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class AutocompleteTests {
+    private List<String> words() {
+        return new ArrayList<String>(
+                Arrays.asList(
+                        "hello",
+                        "high",
+                        "seattle",
+                        "seatac",
+                        "see",
+                        "hollow",
+                        "how"));
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+
+
 }
